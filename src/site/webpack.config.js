ed9c487279eb85e0de9dd94c064c138e3bda2394
gcastellov <dreeyfus20@hotmail.com>
2021-03-16 19:13:42 +0100

    showing version number to UI

--- src/site/webpack.config.js
+++ src/site/webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
-module.exports = {
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/dist/"
-  },
-  mode: "development"
+const webpack = require('webpack');
+const dotenv = require('dotenv').config({
+  path: path.join(__dirname, '.env')
+});
+
+module.exports = () => {
+
+  console.log("Building version:" + dotenv.parsed.VERSION);
+
+  return {
+    entry: "./index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/dist/"
+    },
+    mode: "development",
+    plugins: [
+      new webpack.DefinePlugin({
+        APP_VERSION: JSON.stringify(dotenv.parsed.VERSION)
+      })
+    ]
+  };
 };
