4c154179daef8e0b19913e27a536560c3da2219e
gcastellov <dreeyfus20@hotmail.com>
2022-01-12 19:54:21 +0100

    factory to create either dictionarylist or dictionarymaker

--- src/ripper_wasm/src/internals/management.rs
+++ src/ripper_wasm/src/internals/management.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
-use super::dictionarylist::{DictionaryList};
+use super::{dictionarylist::{DictionaryList}, dictionarymaker::DictionaryMaker};
 
 #[wasm_bindgen]
 pub struct DictionaryManager {
     dictionary_cache: HashMap<String, Vec<String>>,
     dictionary_selection: Vec<String>,
+    dictionary_type: DictionaryType,
 }
 
 pub trait Dictionary: Iterator {
@@ -15,6 +16,11 @@ pub trait Dictionary: Iterator {
     fn get_chunk(&mut self, size: usize) -> Option<&[String]>;
     fn forward(&mut self, size: usize);
     fn get_last(&self) -> Option<String>;
+}
+
+enum DictionaryType {
+    List = 0,
+    Maker = 1,
 }
 
 #[wasm_bindgen]
@@ -44,18 +50,31 @@ impl DictionaryManager {
     pub fn load_dictionaries(&mut self, keys: Vec<JsValue>) {
         self.dictionary_selection = keys.iter().filter_map(|k| k.as_string()).collect();
     }
+
+    pub fn set_type(&mut self, dictionary_type: u8) {
+        self.dictionary_type = match dictionary_type {
+            1 => DictionaryType::Maker,
+            _ => DictionaryType::List
+        };
+    }
+
 }
 
 impl DictionaryManager {
     pub fn make(&self) -> Box<dyn Dictionary<Item=String>> {
-        let entries: Vec<String> = self
-            .dictionary_selection
-            .iter()
-            .filter_map(|key|self.dictionary_cache.get(key))
-            .flat_map(|word| word.to_owned())
-            .collect();
-
-        Box::new(DictionaryList::new(&entries))
+        match self.dictionary_type {
+            DictionaryType::List => {
+                let entries: Vec<String> = self
+                .dictionary_selection
+                .iter()
+                .filter_map(|key|self.dictionary_cache.get(key))
+                .flat_map(|word| word.to_owned())
+                .collect();
+    
+                Box::new(DictionaryList::new(&entries))
+            },
+            DictionaryType::Maker => Box::new(DictionaryMaker::new(16, &(32..128).filter_map(char::from_u32).collect()))
+        }
     }
 
     fn extract(entries: &str) -> Vec<String> {
@@ -79,6 +98,7 @@ impl Default for DictionaryManager {
         Self {
             dictionary_cache: HashMap::default(),
             dictionary_selection: Vec::default(),
+            dictionary_type: DictionaryType::List,
         }
     }
 }
